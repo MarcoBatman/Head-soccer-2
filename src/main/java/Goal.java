b2b54a3bcc6add50dc10e233aecfa814edccf577
Beckudrup <de3gutter@gmail.com>
2021-01-11 10:11:22 +0100

    Ved ikke hvad dette er men ok?

--- src/main/java/Goal.java
+++ src/main/java/Goal.java
@@ -1,2 +1,21 @@
+import processing.core.PApplet;
+
 public class Goal {
+
+PApplet p;
+int x,y,w,h;
+
+
+    Goal(PApplet p, float x, float y, float w, float h){
+
+
+    }
+
+    public void displayGoal{
+        p.rect(x,y,w,h);
+            
+    }
+    public void insideGoal{
+
+    }
 }
