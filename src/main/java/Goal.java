d926aaea7bba195ff0afd740f2627a5734078080
Beckudrup <de3gutter@gmail.com>
2021-01-11 10:25:08 +0100

    df

--- src/main/java/Goal.java
+++ src/main/java/Goal.java
@@ -16,7 +16,16 @@ int x,y,w,h;
     }
 
     public void displayGoal(){
+        //Spiller 1 mål
         p.rect(0,700,50,20);
+        p.rect(0,1000,20,20);
+        p.rect(0,1000,50,20);
+
+        //Spiller 2 mål
+        p.rect(1000,700,-50,-20);
+        p.rect(1000,1000,-20,-20);
+        p.rect(1000,1000,-50,-20);
+
 
     }
     public void insideGoal(){
