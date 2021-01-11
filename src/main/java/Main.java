d790762e7297e9dfa206bd09acdc7f98e5ce74a4
MarcoBatman <54103563+MarcoBatman@users.noreply.github.com>
2021-01-11 09:33:43 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) { PApplet.main("Main"); }
+
+    @Override
+    public void settings() {
+        super.settings();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+}
