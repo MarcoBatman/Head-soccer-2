b2c3ca9b7f435d02d617f47fde6da054e333ee47
MarcoBatman <Marco@terrida.dk>
2021-01-15 10:32:12 +0100

    gsdfgsd

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -5,34 +5,53 @@ public class Character {
     PApplet p;
     PVector pos;
     PVector vel;
-    PVector resistanceX = new PVector((float) -0.5,(float) 0);
-    PVector resistanceY = new PVector((float) 0,(float) -0.3);
-    int player;
+    PVector resistanceX = new PVector((float) -0.5, (float) 0);
+    PVector resistanceY = new PVector((float) 0, (float) 0.3);
+    boolean up, down, left, right;
+    int playernr;
 
-    Character(PApplet p, int posX, int posY, int player){
-    this.p = p;
-    this.pos=new PVector(posX,posY);
-    this.player = player;
+    Character(PApplet p, int posX, int posY, int player) {
+        this.p = p;
+        this.pos = new PVector(posX, posY);
+        this.playernr = player;
     }
 
-    void move(){
-    pos.add(vel);
-    if(vel.x>0)
-    vel.add(resistanceX);
-    if(vel.y>0)
-        vel.add(resistanceY);
-    if(vel.x<0)
-        vel.x=0;
-    if(vel.y<0)
-        vel.y=0;
+    void move() {
+        pos.add(vel);
+        if (up)
+            vel.y = -7;
+        if (left)
+            vel.x = -5;
+        if (right)
+            vel.x = 5;
+        if (pos.y < 0)
+            pos.y = 0;
+        if (vel.x > 0)
+            vel.add(resistanceX);
+        if (vel.x < 0)
+            vel.sub(resistanceX);
+        if (vel.y > 0)
+            vel.add(resistanceY);
+        if (playernr == 1) {
+            if (pos.x <= 0) {
+                vel.x = 0;
+                pos.x = 0;
+            }
+        }
+        if (playernr == 2) {
+            if (pos.x >= p.width) {
+                vel.x = 0;
+                pos.x = p.width;
+            }
+        }
+        if (pos.y > p.height)
+            vel.y = 0;
     }
 
-    void display(){
-
+    void display() {
+        p.rect(pos.x, pos.y, 30, 100);
 
     }
-
-
 
 
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,7 +2,37 @@ import processing.core.PApplet;
 
 public class Player extends Character {
 
-    Player(PApplet p, int posX, int posY) {
-        super(p, posX, posY);
+    Player(PApplet p, int posX, int posY,int playernr) {
+        super(p, posX, posY, playernr);
     }
+
+
+
+
+    boolean setMove(int k, boolean b) {
+
+
+        switch (k) {
+          case +'W':
+          case 38:
+            return up = b;
+
+          case +'S':
+          case 40:
+            return down = b;
+
+            case +'A':
+            case 37:
+                return left = b;
+
+            case +'D':
+            case 39:
+                return right = b;
+
+            default:
+                return b;
+        }
+
+    }
+
 }
