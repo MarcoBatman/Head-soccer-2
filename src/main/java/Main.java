537843ee0906ea49bbd6c220d94918219004bd4f
MarcoBatman <Marco@terrida.dk>
2021-01-20 14:18:56 +0100

    fopgbj.øæp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,6 +28,7 @@ public class Main extends PApplet {
         player1.move();
         player2.display();
         player2.move();
+        
     }
     public void mouseClicked(){
         background.helpMenu();
